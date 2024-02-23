7304f6887e9041d7e245381749b9d055f154bb0b
ChanghaoLi <cl580@duke.edu>
2024-02-22 19:01:11 -0800

    add unit test to verify CI pipeline

--- server/src/test/java/org/elasticsearch/common/util/CopyOnFirstWriteMapTests.java
+++ server/src/test/java/org/elasticsearch/common/util/CopyOnFirstWriteMapTests.java
@@ -9,7 +9,9 @@
 package org.elasticsearch.common.util;
 
 import org.elasticsearch.test.ESTestCase;
+import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -17,6 +19,37 @@ import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.sameInstance;
 
 public class CopyOnFirstWriteMapTests extends ESTestCase {
+
+    @Test
+    public void testMapIsEmptyMethod() {
+        var source = new HashMap<String, String>();
+        var copyOnFirstWrite = new CopyOnFirstWriteMap<>(source);
+        assertTrue(copyOnFirstWrite.isEmpty());
+    }
+    @Test
+    public void testMapSizeMethod() {
+        var source = new HashMap<String, String>();
+        var copyOnFirstWrite = new CopyOnFirstWriteMap<>(source);
+        int size = copyOnFirstWrite.size();
+        assertEquals(0, size);
+    }
+
+    public void testMapContainsKeyMethod() {
+        var source = new HashMap<String, String>();
+        var copyOnFirstWrite = new CopyOnFirstWriteMap<>(source);
+        copyOnFirstWrite.put("key1", "value1");
+        assertTrue(copyOnFirstWrite.containsKey("key1"));
+    }
+
+
+    public void testMapContainsValueMethod() {
+        var source = new HashMap<String, String>();
+        var copyOnFirstWrite = new CopyOnFirstWriteMap<>(source);
+        copyOnFirstWrite.put("key1", "value1");
+        assertTrue(copyOnFirstWrite.containsValue("value1"));
+    }
+
+
 
     public void testShouldNotCopyIfThereWereNoUpdates() {
         var source = Map.of("key", "value");
